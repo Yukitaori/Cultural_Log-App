a6825214de9494bfb5d94e2c7756780f7af97cce
Francois Chavallard <francois.chavallard@gmail.com>
2023-07-17 19:47:07 +0200

    preparation routes post entités

--- backend/src/router.js
+++ backend/src/router.js
@@ -18,12 +18,16 @@ router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
 router.get("/books", bookControllers.browse);
+router.post("/books", hashPassword, bookControllers.add);
 
 router.get("/comics", comicControllers.browse);
+router.post("/comics", hashPassword, comicControllers.add);
 
 router.get("/discs", discControllers.browse);
+router.post("/discs", hashPassword, discControllers.add);
 
 router.get("/movies", movieControllers.browse);
+router.post("/movies", hashPassword, movieControllers.add);
 
 router.get("/users", userControllers.browse);
 router.post("/users", hashPassword, userControllers.add);
