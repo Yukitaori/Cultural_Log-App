91e9734df628d65348f41bd9218fd5752b6019ed
Francois Chavallard <francois.chavallard@gmail.com>
2023-07-18 13:33:36 +0200

    mise en place du hook useLocalStorage + contexte userContext

---
+++ frontend/src/contexts/UserContext.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+import { createContext, useMemo, useContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+import instance from "../services/APIService";
+
+const UserContext = createContext();
+
+export default UserContext;
+
+export function UserContextProvider({ children }) {
+  const [user, setUser] = useLocalStorage("user", null);
+  const login = (_user) => {
+    setUser(_user);
+  };
+  const logout = () => {
+    instance.get("/logout");
+    setUser(null);
+  };
+
+  const value = useMemo(
+    () => ({
+      user,
+      setUser,
+      login,
+      logout,
+    }),
+    [user]
+  );
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+}
+
+export const useUserContext = () => useContext(UserContext);
+
+UserContextProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
